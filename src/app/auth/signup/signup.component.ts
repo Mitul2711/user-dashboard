7b893d3c8409a8b449bfaa6a6d006a1d7ed1e25c
Mitul <130025869+Mitul2711@users.noreply.github.com>
2025-06-23 04:43:56 +0000

    sign up logic updated

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
@@ -11,11 +11,12 @@ import { CommonService } from '../../services/common.service';
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
   showPassword = false;
   showConfirmPassword = false;
   formSubmitted = false;
   signupForm!: FormGroup;
+  userDetails: any[] = [];
 
   constructor(private fb: FormBuilder, private authService: AuthService, private commonService: CommonService, private router: Router) {
     this.signupForm = this.fb.group({
@@ -28,6 +29,10 @@ export class SignupComponent {
     })
   }
 
+  ngOnInit(): void {
+    this.getUserDetails();
+  }
+
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
@@ -36,29 +41,54 @@ export class SignupComponent {
     this.showConfirmPassword = !this.showConfirmPassword;
   }
 
+  getUserDetails() {
+    this.authService.getUsers().subscribe((res: any) => {
+      this.userDetails = res;
+    });
+  }
+
   onSubmit() {
     this.formSubmitted = true;
+    const formData = this.signupForm.value;
     if (this.signupForm.valid) {
-      const formData = this.signupForm.value;
-      if (formData.password === formData.confirmPass) {
-        let data = {
-          userName: formData.userName,
-          password: formData.password
-        }
-        this.authService.postUser(data).subscribe({
-          next: (res) => {
-            this.commonService.showSuccess('User created successfully');
-            this.router.navigate(['/login']);
-            this.signupForm.reset();
-          },
-          error: (err) => {
-            console.error('Error creating user:', err);
-          }
-        });
-
+      if (this.userDetails.length == 0) {
+        this.postUser();
       } else {
-        this.commonService.showError('Passwords do not match!');
+        const userExists = this.userDetails.some(
+          (user: any) => user.userName === formData.userName
+        );
+
+        if (userExists) {
+          this.commonService.showError('Username already taken. Please choose another.');
+          return;
+        } else {
+          this.postUser();
+        }
+
       }
+    }
+  }
+
+  postUser() {
+    const formData = this.signupForm.value;
+    if (formData.password === formData.confirmPass) {
+      let data = {
+        userName: formData.userName,
+        password: formData.password
+      }
+      this.authService.postUser(data).subscribe({
+        next: (res) => {
+          this.commonService.showSuccess('User created successfully');
+          this.router.navigate(['/login']);
+          this.signupForm.reset();
+        },
+        error: (err) => {
+          console.error('Error creating user:', err);
+        }
+      });
+
+    } else {
+      this.commonService.showError('Passwords do not match!');
     }
   }
 
